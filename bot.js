594b40140055bf016e050b1515e3bf970eb7614c
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-08-17 23:17:21 -0500

    I forgot to add the changes of this file.

--- bot.js
+++ bot.js
@@ -4,7 +4,12 @@ const data = require("./data.json");
 const fs = require("fs");
 const client = new Discord.Client();
 const prefix = "/";
+const lang = "es";
 let manual
+
+const Language = require("./Services/LanguageSetter.js");
+var messages = Language(lang);
+
 
 (()=>{
     fs.readFile("./manual.md", "utf-8", (err, data)=>{
@@ -17,23 +22,42 @@ let manual
 client.on("ready", () => console.log('¡Team Roulette iniciado!'))
 
 client.on("message", message => {
-    const command =message.content, roulette = new Roulette(data.names)
+
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase(), roulette = new Roulette(data.names, messages)
+
     if (message.author.bot || !message.content.startsWith(prefix)) return
     switch (command){
-        case "/info":
+        case 'restartbot':
+            message.channel.send(messages.restartMessage).then(() => process.exit(1));
+            break
+        case "info":
             message.channel.send(manual)
             break
-        case "/lista":
+        case "lista":
             message.channel.send(roulette.random())
             break
-        case '/parejas':
+        case 'parejas':
             message.channel.send(roulette.couples())
             break
-        case '/tercias':
+        case 'tercias':
             message.channel.send(roulette.threesomes())
             break
+        case 'create':
+            if(args.length > 0){
+                message.channel.send(roulette.create())
+            }
+            else {
+                message.channel.send("")
+            }
+            
+            break
         default:
-            message.channel.send(`Comando desconocido.\n ${manual}`)
+            message.channel.send(messages.invalidCommand)
     }
 })
 
